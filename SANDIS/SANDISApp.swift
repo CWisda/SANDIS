8a9e0119261673eb9a9d737f9dc759bc37f5f549
Charles Wisda <crwisda@gmail.com>
2025-03-18 15:56:56 -0700

    Initial Commit

---
+++ SANDIS/SANDISApp.swift
@@ -0,0 +1,17 @@
+//
+//  SANDISApp.swift
+//  SANDIS
+//
+//  Created by Charles Wisda on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SANDISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SANDISTests/SANDISTests.swift
@@ -0,0 +1,16 @@
+//
+//  SANDISTests.swift
+//  SANDISTests
+//
+//  Created by Charles Wisda on 3/18/25.
+//
+
+import Testing
+
+struct SANDISTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
